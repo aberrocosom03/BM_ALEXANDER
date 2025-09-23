192552a0c92e249e4a428e12e20a4fee5b0e4a11
Alexander <aberrocosom03@edcarex.es>
2025-09-23 10:20:21 +0200

    Trabajo Nombre y Usuario

---
+++ src/UNIDAD_1/Nombre_Usuario.java
@@ -0,0 +1,19 @@
+package UNIDAD_1;
+
+import java.util.Scanner;
+
+public class Nombre_Usuario {
+
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+		 System.out.print("Introduce tu nombre: ");
+		 String nombre = sc.nextLine();
+		 
+		 System.out.print("Introduce tu edad: ");
+		 int edad = sc.nextInt();
+		 
+
+		 System.out.println("Hola "+nombre+", tienes "+edad+" años");
+		 }
+
+}
