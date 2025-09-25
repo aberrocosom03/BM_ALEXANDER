8745c277987190973146c529a3fb6e154eca6bb5
Alexánder <aberrocosom03@educarex.es>
2025-09-25 10:13:05 +0200

    Practia declaración, salida Y Modificación de valores en Java

---
+++ src/UNIDAD_1/Declaracion_Salida_Y_Mod_Valores.java
@@ -0,0 +1,44 @@
+package UNIDAD_1;
+
+public class Declaracion_Salida_Y_Mod_Valores {
+
+
+
+	 public static void main(String[] args) {
+	 // Enteros
+	 int edad = 19;
+	 long poblacion = 9000000L;
+	 // Decimales
+	 float precio = 9.95f;
+	 double pi = 3.1415926535;
+	 // Carácter
+	 char inicial = 'A';
+	 // Booleano
+	 boolean aprobado = true;
+	 System.out.println("Edad: " + edad);
+	 System.out.println("Población: " + poblacion);
+	 System.out.println("Precio: " + precio);
+	 System.out.println("PI: " + pi);
+	 System.out.println("Inicial: " + inicial);
+	 System.out.println("¿Aprobado? " + aprobado);
+	 
+	//Enteros
+		 int contador=0;
+		 System.out.println("contador: " + contador);
+		 contador=15;
+		 System.out.println("contador: " + contador);
+		 contador=contador-3;
+		 System.out.println("contador: " + contador);
+		 contador++;
+		 System.out.println("contador: " + contador);
+		 contador--;
+		 System.out.println("contador: " + contador);
+		
+		 
+		 
+	 }
+	 
+	 
+}
+	
+	
