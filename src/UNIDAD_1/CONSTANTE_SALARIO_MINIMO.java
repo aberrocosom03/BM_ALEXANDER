c01c6eda23b5e32ff60922aa0c981de4bd3e3cdf
Alexánder <aberrocosom03@educarex.es>
2025-09-29 11:13:40 +0200

    PRÁCTICA SALARIO MINIMO

---
+++ src/UNIDAD_1/CONSTANTE_SALARIO_MINIMO.java
@@ -0,0 +1,26 @@
+package UNIDAD_1;
+
+public class CONSTANTE_SALARIO_MINIMO {
+
+		 public static void main(String[] args) {
+			 
+		 // Uso de literales
+		 int numero = 1080; // literal entero
+		 System.out.println("Salario Minimo : " + numero);
+		 
+		 int numero1 = 0x1A;
+		 System.out.println(numero1);
+		 char omega = '\u03A9';
+		 System.out.println(omega);
+		 double exp = 1.2e3;
+		 System.out.println("El resultado es : " + exp); 
+		 String mensaje = "Alexander";
+		 System.out.println(mensaje); 
+		 
+		 final double RADIO = 6;
+		 final double PI = 3,14;
+		 
+		 
+		 }
+}
+	
