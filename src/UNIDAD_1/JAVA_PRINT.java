eb25f33fd2a3c642ea6cf04ff6540f04443d687c
Alexánder <aberrocosom03@educarex.es>
2025-10-02 10:51:03 +0200

    Práctica Java print

---
+++ src/UNIDAD_1/JAVA_PRINT.java
@@ -0,0 +1,17 @@
+package UNIDAD_1;
+
+public class JAVA_PRINT {
+	
+public static void main(String [] args) {
+	
+	//PRINT JAVA
+	System.out.print("Hola ");
+	System.out.println("Mundo ");
+	System.out.printf("El numero es: %d\n", 42);
+	
+	//PRINT SECUENCIA 
+	double precio = 12; 
+	System.out.printf("El precio es: %f" , precio ); 
+	
+	}
+}
