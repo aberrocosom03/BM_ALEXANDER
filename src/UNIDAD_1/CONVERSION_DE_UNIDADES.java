f253428d551a60c7e91e193c8b535e16a56fb006
Alexánder <aberrocosom03@educarex.es>
2025-10-02 11:04:29 +0200

    Práctica conversión de unidades

---
+++ src/UNIDAD_1/CONVERSION_DE_UNIDADES.java
@@ -0,0 +1,14 @@
+package UNIDAD_1;
+
+public class CONVERSION_DE_UNIDADES {
+	
+	public static void main(String[] args) {
+		
+		Scanner teclado = new Scanner (System. int);
+		
+		System.out.println("Introduce la temperatura en grados celsius");
+		double celsius = teclado 
+		double farenhait = t
+		
+	}
+}
